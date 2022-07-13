3c82f7344baab7fa2ec58ee009eb5c93326bc830
Sunday Robert <92962171+efyzhi@users.noreply.github.com>
2022-07-13 13:24:53 +0000

    challenge 2 and challenge 3

--- src/assignment.js
+++ src/assignment.js
@@ -30,6 +30,8 @@ function sumOfNumbers(arrayOfNumbers) {
 function countEvenNumbers(arrayOfNumbers) {
   return arrayOfNumbers.length;
 } 
+//assignment.countEvenNumbers = countEvenNumbers;
+
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
 
 const countEvenNumber = numbers.filter(function(evenNumbers){
@@ -50,8 +52,12 @@ console.log(countEvenNumber);
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
+
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers                };
+    return arrayOfNumbers
+};
+//assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+
 const celsiusDegree = [-50, -45, -32, -27, -23, -18, 0, 22, 50, 47, 100];
 
 const celsiusToFahrenheits = celsiusDegree.map(function(celsius){

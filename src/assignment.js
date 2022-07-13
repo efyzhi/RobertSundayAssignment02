0d00548a028eeda984a8b411c8e7c3596b9f823e
Sunday Robert <92962171+efyzhi@users.noreply.github.com>
2022-07-13 15:16:05 +0000

    challenge 2 and chalenge 3

--- src/assignment.js
+++ src/assignment.js
@@ -30,13 +30,13 @@ function sumOfNumbers(arrayOfNumbers) {
 function countEvenNumbers(arrayOfNumbers) {
   return arrayOfNumbers.length;
 } 
-//assignment.countEvenNumbers = countEvenNumbers;
+// assignment.countEvenNumbers = countEvenNumbers;
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
-
+ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
 const countEvenNumber = numbers.filter(function(evenNumbers){
   return evenNumbers % 2 == 0;
 });
+
 /**
  * Challenge - 3
  * 
@@ -53,15 +53,18 @@ const countEvenNumber = numbers.filter(function(evenNumbers){
  */
 
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers
+    return arrayOfNumbers  
 };
-//assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+//  assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 const celsiusDegree = [-50, -45, -32, -27, -23, -18, 0, 22, 50, 47, 100];
 
 const celsiusToFahrenheits = celsiusDegree.map(function(celsius){
   return Math.trunc((celsius * 9/5) + 32)
-});   
+});  
+
+
+
 // ========================
 // DO NOT EDIT THIS BLOCK
 module.exports = assignment;
